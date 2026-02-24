5e59453ab0e3618c4649ccb0b23b202000acaf49
Hitesh Prajapati <hp@Hiteshs-MacBook-Air.local>
2026-02-24 18:05:57 +0530

    view details

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 function ProductList({products}) {
+  const navigate = useNavigate();
   return (
     <>
       <div>ProductList</div>
@@ -22,6 +25,14 @@ function ProductList({products}) {
                   <p class="card-text">
                     {product.description.slice(0, 30) + "..."}
                   </p>
+                  <div className="d-grid gap-2">
+                    <button type="button" className="btn btn-primary"
+                    onClick={()=>navigate(`/product-details/${product.id}`)}
+                    >
+                      View Details
+                    </button>
+                  </div>
+                  
                 </div>
               </div>
             </div>
