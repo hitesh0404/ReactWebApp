84c71ba05e7675ac5dae5b21249c5ef64ce6d984
Hitesh Prajapati <hp@Hiteshs-MacBook-Air.local>
2026-02-25 18:01:54 +0530

    red color

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,34 +1,50 @@
 import { useForm } from "react-hook-form";
 
 function LoginForm() {
-    const {
-      register,
-      handleSubmit,
-      watch,
-      formState: { errors },
-    } = useForm();
-    const onSubmit = (data)=>{
-        console.log(data);
-    }
-    
-  return (<>
-    <div>LoginForm</div>
-    <form  onSubmit={handleSubmit(onSubmit)}>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  function isValid(password) {
+    if (!password)               return "this field is required";
+    if (password.length < 8)     return "min length of password should be greater then 8";
+    if (password.length > 16)    return "Max length of password should be less then 16";
+    if (!/[0-9]/.test(password)) return "number required";
+    if (!/[A-Z]/.test(password)) return "uppercase required";
+    if (!/[a-z]/.test(password)) return "lowercase required";
+  }
+  return (
+    <>
+      <div>LoginForm</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username" , {required : true})} />
-        <p>{errors.username && errors.username?.message }</p>
+        <input
+          type="text"
+          id="username"
+          {...register("username", { required: true })}
+        />
+        <p className="errors">{errors.username && errors.username?.message}</p>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" {...register("password" ,
-        {
-            required :{ 
+        <input
+          type="password"
+          id="password"
+          {...register("password", {
+            required: {
               value: true,
-              message:"this field is required"},
-
-        })} />
-        {errors.password  && errors.password.message}
+              message: "this field is required",
+            },
+            validate: isValid,
+          })}
+        />
+       <p className="errors"> {errors.password && errors.password.message}</p>
         <button type="submit">Submit</button>
-    </form>
-  </>
-  )
+      </form>
+    </>
+  );
 }
-export default LoginForm
+export default LoginForm;
